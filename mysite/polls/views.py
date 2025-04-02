014d6eb7dec342bc12466c3de4347bfcfad87dcd
Filip <filip.szmelcz@gmail.com>
2025-04-02 08:47:02 +0200

    Change 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -16,7 +16,19 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
+
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+     return HttpResponse("You're looking at question %s." % question_id)
+     question = get_object_or_404(Question, pk=question_id)
+     return render(request, "polls/detail.html", {"question": question})
+ 
+ def results(request, question_id):
+     response = "You're looking at the results of question %s."
